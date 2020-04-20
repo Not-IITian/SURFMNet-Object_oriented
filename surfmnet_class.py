da502d7c42a624f6e873b52606d1ecae42918630
Abhishek Sharma <abhishek.abhi57838@gmail.com>
2020-04-20 03:18:53 +0200

    Add files via upload

---
+++ surfmnet_class.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 11 21:16:02 2020
+
+@author: abhishek
+"""
+import os
+import sys
+import tensorflow as tf
+BASE_DIR = os.path.dirname(__file__)
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../utils'))
+
+from pointnet_util import pointnet_sa_module, pointnet_fp_module
+
+#using global functions inside a class..you can just use them as such.
+class surfmnet:
+    def __init__(self, SURFMNetConfig):
+        self.num_evecs = SURFMNetConfig["num_evecs"]
+        self.pc_s = tf.placeholder(tf.float32,shape=(None, None, 3))
+        self.pc_t = tf.placeholder(tf.float32,shape=(None, None, 3))
+        self.source_evecs = tf.placeholder(tf.float32, shape=(None, None, self.num_evecs))
+        self.source_evecs_trans = tf.placeholder(tf.float32,shape=(None, self.num_evecs, None))
+        self.source_evals = tf.placeholder(tf.float32,shape=(None, self.num_evecs))
+        self.target_evecs = tf.placeholder(tf.float32,shape=(None, None, self.num_evecs))
+        self.target_evecs_trans = tf.placeholder(tf.float32,shape=(None, self.num_evecs, None))
+        self.target_evals = tf.placeholder(tf.float32,shape=(None, self.num_evecs))
+        # train\test switch flag
+        self.phase = tf.placeholder("bool", None) 
+        self.pc_size = SURFMNetConfig["pc_size"]
+        self.dims = SURFMNetConfig["dims"]
+        self.num_fclayers = SURFMNetConfig['num_fclayers']
+        batch = tf.Variable(0)
+        #self.bn_decay = get_bn_decay(batch)
+        with tf.variable_scope('siamese') as scope:
+            o1 = self.model(self.pc_s)
+            scope.reuse_variables()
+            o2 = self.model(self.pc_t)
+                  
+        
+        
+        self.C_est_AB = self.solve_ls(o1,o2)
+        self.C_est_BA = self.solve_ls(o2,o1)
+        self.cost = self.func_map_layer()
+        
+    
+    def solve_ls(self,A, B):    
+    # Transpose input matrices
+        At = tf.transpose(A, [0, 2, 1])
+        Bt = tf.transpose(B, [0, 2, 1])
+
+    # Solve C via least-squares
+        Ct_est = tf.matrix_solve_ls(At, Bt)
+    #Ct_est = tf.matrix_solve_ls(At, Bt, l2_regularizer = 0.000001)
+        C_est = tf.transpose(Ct_est, [0, 2, 1], name='C_est')
+   
+        return C_est
+
+    def res_layer(self,x_in, dims_out, scope, phase):
+        with tf.variable_scope(scope):
+            x = tf.contrib.layers.fully_connected(x_in, dims_out,activation_fn=None,scope='dense_1')
+            x = tf.contrib.layers.batch_norm( x,center=True, scale=True,is_training=phase,
+                                                variables_collections=["batch_norm_non_trainable_variables_collection"],
+                                                scope='bn_1')
+            x = tf.nn.relu(x, 'relu')
+            x = tf.contrib.layers.fully_connected(x,dims_out,activation_fn=None,scope='dense_2')
+            x = tf.contrib.layers.batch_norm( x,center=True,scale=True,is_training=phase,
+                                                variables_collections=["batch_norm_non_trainable_variables_collection"],
+                                                scope='bn_2')
+            
+            # If dims_out change, modify input via linear projection
+            # (as suggested in resNet)
+            if not x_in.get_shape().as_list()[-1] == dims_out:
+                x_in = tf.contrib.layers.fully_connected( x_in, dims_out, activation_fn=None,scope='projection')
+            x += x_in
+            return tf.nn.relu(x)
+  
+    def model(self,pc_1):
+    #source_evecs, source_evecs_trans, source_evals, target_evecs, target_evecs_trans, target_evals, bn_decay=None):
+        dims=self.dims
+        """ Semantic segmentation PointNet, input is BxNx3, output Bxnum_class """        
+        phase= self.phase
+        
+        net = {}    
+        for i_layer in range(self.num_fclayers):
+            with tf.variable_scope("layer_%d" % i_layer) as scope:
+                if i_layer == 0:
+                    net['fclayer_%d' % i_layer] = self.res_layer(pc_1, dims_out = 128, scope= scope,phase= phase)
+                else:
+                    net['fclayer_%d' % i_layer] = self.res_layer(net['fclayer_%d' % (i_layer-1)], dims_out = int(dims[i_layer]),scope= scope, phase= phase)
+                                           
+        #  Project output features on the shape Laplacian eigen functions
+        
+        layer_C_est = i_layer + 1   # Grab current layer index
+        F = net['fclayer_%d' % (layer_C_est-1)]
+        A = tf.matmul(self.source_evecs_trans, F) # why trans
+        #net['A'] = A
+        return A
+                    
+    #use a parameter with self. and use a memeber function also with self.
+    def penalty_bijectivity(self): 
+        x_1 = tf.nn.l2_loss(tf.subtract(tf.matmul(self.C_est_AB, self.C_est_BA),tf.eye(tf.shape(self.C_est_AB)[1])))
+        x_2 = tf.nn.l2_loss(tf.subtract(tf.matmul(self.C_est_BA, self.C_est_AB),tf.eye(tf.shape(self.C_est_BA)[1])))
+   
+        return (x_1+x_2)
+
+    def penalty_ortho(self):   
+        x_1 = tf.nn.l2_loss(tf.subtract(tf.matmul(tf.transpose(self.C_est_AB, perm=[0, 2, 1]),self.C_est_AB),tf.eye(tf.shape(self.C_est_AB)[1])))
+        x_2 = tf.nn.l2_loss(tf.subtract(tf.matmul(tf.transpose(self.C_est_BA, perm=[0, 2, 1]),self.C_est_BA),tf.eye(tf.shape(self.C_est_BA)[1])))
+        return (x_1 +x_2)
+
+    def penalty_laplacian_commutativity(self):    
+    # Quicker and less memory than taking diagonal matrix
+        eig1 = tf.einsum('abc,ac->abc', self.C_est_AB, self.source_evals)
+        eig2 = tf.einsum('ab,abc->abc', self.target_evals, self.C_est_AB)
+        eig3 = tf.einsum('abc,ac->abc', self.C_est_BA, self.source_evals)
+        eig4 = tf.einsum('ab,abc->abc', self.target_evals, self.C_est_BA)
+        return tf.nn.l2_loss(tf.subtract(eig2, eig1)) + tf.nn.l2_loss(tf.subtract(eig4, eig3))
+
+    def func_map_layer(self):
+         alpha = 10**3  # Bijectivity
+         beta = 10**3   # Orthogonality
+         gamma = 1      # Laplacian commutativity
+    
+         E1 = self.penalty_bijectivity()/2
+
+         E2 = self.penalty_ortho()/2
+
+         E3 = self.penalty_laplacian_commutativity()/2
+                                                                                 
+         loss = tf.reduce_mean(alpha * E1 + beta * E2 + gamma * E3 )
+    
+     #check this..
+         loss /= tf.to_float(tf.shape(self.C_est_AB)[1] * tf.shape(self.C_est_AB)[0])   
+
+         return loss
+              
---
+++ tr_surfmnet_class.py
@@ -0,0 +1,346 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 02:06:44 2019
+
+@author: abhishek
+"""
+import os
+import time
+import numpy as np
+from scipy.spatial import cKDTree
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+import tensorflow as tf
+import scipy.io as sio
+from surfmnet_class import *
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+# Training parameterss
+flags.DEFINE_float('learning_rate', 1e-4, 'initial learning rate.')
+flags.DEFINE_integer('batch_size', 4, 'batch size.')
+# Architecture parameters
+flags.DEFINE_integer('num_fclayers', 2, 'network depth') 
+flags.DEFINE_integer('num_evecs', 30, "number of eigenvectors used for representation")
+# Data parameters
+flags.DEFINE_string('targets_dir', '../../Unsupervised_FMnet/Shapes/Surreal_nicolas/MAT/','directory with shapes')  
+flags.DEFINE_string('feat_dir', '../../Unsupervised_FMnet/Shapes/Surreal_nicolas/MAT/','directory with shapes')  
+flags.DEFINE_string('files_name', 'surreal_', 'name common to all the shapes')
+
+flags.DEFINE_string('targets_dir_te', '../../Unsupervised_FMnet/Shapes/Scape_r_aligned/MAT/','directory with shapes')  
+flags.DEFINE_string('feat_dir_te', '../../Unsupervised_FMnet/Shapes/Scape_r_aligned/scape_mat/','directory with shapes')  
+flags.DEFINE_string('files_name_te', '', 'name common to all the shapes')
+
+flags.DEFINE_integer('max_train_iter', 12000, '')
+flags.DEFINE_integer('num_vertices', 4000, '') 
+flags.DEFINE_integer('save_summaries_secs', 500, '') 
+flags.DEFINE_integer('save_model_secs', 500, '')
+flags.DEFINE_string('log_dir_', 'Training/SCAPE_r_aligned/pointnet_fmnet_b4_lr_-4_30evec_del-0_4k_2fc_aligned_tr-Sur_50_dm/',
+                    'directory to save models and results')
+flags.DEFINE_string('matches_dir', 'Matches/SCAPE_r_aligned/pointnet_fmnet_b4_lr_-4_30evec_del-0_4k_2fc_aligned_tr_Sur_50_dm/',
+                    'directory to matches')
+flags.DEFINE_integer('dim_', 128,'')
+flags.DEFINE_integer('decay_step', 200000, help='Decay step for lr decay [default: 200000]')
+
+flags.DEFINE_float('decay_rate', 0.7, help='Decay rate for lr decay [default: 0.7]')
+
+# Globals
+dim_=FLAGS.dim_   
+flags.DEFINE_list('dims', [dim_,dim_,dim_,dim_, dim_, dim_, dim_], '')      
+       
+#dim_1_layer = int(FLAGS.dims[0])
+#flags.DEFINE_integer('dim_shot', dim_1_layer, '')  
+#no_layers = FLAGS.num_fclayers
+
+#last_layer = int(FLAGS.dims[no_layers-1])
+#flags.DEFINE_integer('dim_out', last_layer, '') 
+	
+vert_dir = FLAGS.feat_dir
+vert_dir_te = FLAGS.feat_dir_te
+n_tr = 50
+train_subjects,test_subjects = (range(50),range(60,70))
+#train_subjects = list(range(n_tr)) + list(range(52,60))
+#test_subjects = range(60,70)
+main_dir = FLAGS.targets_dir
+files_name =FLAGS.files_name
+main_dir_te = FLAGS.targets_dir_te
+files_name_te =FLAGS.files_name_te
+ 
+DECAY_STEP = FLAGS.decay_step
+DECAY_RATE = FLAGS.decay_rate 
+BN_INIT_DECAY = 0.5
+BN_DECAY_DECAY_RATE = 0.5
+BN_DECAY_DECAY_STEP = float(DECAY_STEP)
+BN_DECAY_CLIP = 0.99 
+BATCH_SIZE = FLAGS.batch_size
+	
+SURFMNetConfig = {
+    "pc_size": FLAGS.num_vertices,
+    "dim_out": FLAGS.dims[FLAGS.num_fclayers-1],
+    "num_fclayers":FLAGS.num_fclayers ,
+    "dim_shot": int(FLAGS.dims[0]),
+    "dim_" : FLAGS.dim_,
+    "BATCH_SIZE" : FLAGS.batch_size,
+    "dim_1_layer" :int(FLAGS.dims[0]),
+     "num_evecs" :FLAGS.num_evecs,
+     "dims" : FLAGS.dims     
+}
+def get_input_pair(batch_size, num_vertices, dataset):
+    
+    batch_input = {
+        'source_evecs': np.zeros((batch_size, num_vertices, FLAGS.num_evecs)),
+        'target_evecs': np.zeros((batch_size, num_vertices, FLAGS.num_evecs)),
+        'source_evecs_trans': np.zeros((batch_size,FLAGS.num_evecs,num_vertices)),
+        'target_evecs_trans': np.zeros((batch_size,FLAGS.num_evecs,num_vertices)),
+        'source_shot': np.zeros((batch_size, num_vertices, 3)),
+        'target_shot': np.zeros((batch_size, num_vertices, 3)),
+        'source_evals': np.zeros((batch_size, FLAGS.num_evecs)),
+        'target_evals': np.zeros((batch_size, FLAGS.num_evecs))
+                   }       
+    for i_batch in range(batch_size):                      
+        i_source = train_subjects[np.random.choice(range(n_tr))]
+        i_target = train_subjects[np.random.choice(range(n_tr))]          
+        
+        batch_input_ = get_pair_from_ram(i_target, i_source, dataset)
+        
+        batch_input_['source_labels'] = range(np.shape(batch_input_['source_evecs'])[0])
+        batch_input_['target_labels'] = range(np.shape(batch_input_['target_evecs'])[0])
+        
+        joint_lbls = np.intersect1d(batch_input_['source_labels'],batch_input_['target_labels'])
+        
+        joint_labels_source = np.random.permutation(joint_lbls)[:num_vertices]
+        joint_labels_target = np.random.permutation(joint_lbls)[:num_vertices]
+
+        ind_dict_source = {value: ind for ind, value in enumerate(batch_input_['source_labels'])}
+        ind_source = [ind_dict_source[x] for x in joint_labels_source]
+
+        ind_dict_target = {value: ind for ind, value in enumerate(batch_input_['target_labels'])}
+        ind_target = [ind_dict_target[x] for x in joint_labels_target]
+        
+        message = "number of indices must be equal"
+        assert len(ind_source) == len(ind_target), message
+        
+        evecs_full = batch_input_['source_evecs']
+        #print(evecs_full.shape)
+        evecs= evecs_full[ind_source, :]
+        evecs_trans = batch_input_['source_evecs_trans'][:, ind_source]
+        shot = batch_input_['source_shot'][ind_source, :]
+        #print(batch_input_['target_shot'].shape)                
+        evals = [item for sublist in batch_input_['source_evals'] for item in sublist] # what?
+        batch_input['source_evecs'][i_batch] = evecs
+        batch_input['source_evecs_trans'][i_batch] = evecs_trans
+        batch_input['source_shot'][i_batch] = shot
+        batch_input['source_evals'][i_batch] = evals
+
+        evecs = batch_input_['target_evecs'][ind_target, :]
+        evecs_trans = batch_input_['target_evecs_trans'][:, ind_target]
+        shot = batch_input_['target_shot'][ind_target, :]
+        evals = [item for sublist in batch_input_['target_evals'] for item in sublist]
+        batch_input['target_evecs'][i_batch] = evecs
+        batch_input['target_evecs_trans'][i_batch] = evecs_trans
+        batch_input['target_shot'][i_batch] = shot
+        batch_input['target_evals'][i_batch] = evals
+    return batch_input
+
+
+def get_input_pair_test(i_target, i_source, dataset):
+    batch_input = {}
+    batch_input_ = get_pair_from_ram(i_target, i_source, 'test')
+    evecs = batch_input_['source_evecs']
+    evecs_trans = batch_input_['source_evecs_trans']
+    shot = batch_input_['source_shot']
+    evals = [item for sublist in batch_input_['source_evals'] for item in sublist]
+    batch_input['source_evecs'] = evecs
+    batch_input['source_evecs_trans'] = evecs_trans
+    batch_input['source_shot'] = shot
+    batch_input['source_evals'] = evals
+
+    evecs = batch_input_['target_evecs']
+    evecs_trans = batch_input_['target_evecs_trans']
+    shot = batch_input_['target_shot']
+    evals = [item for sublist in batch_input_['target_evals'] for item in sublist]
+    batch_input['target_evecs'] = evecs
+    batch_input['target_evecs_trans'] = evecs_trans
+    batch_input['target_shot'] = shot
+    batch_input['target_evals'] = evals
+    return batch_input
+
+def get_pair_from_ram(i_target, i_source, dataset):   
+    input_data = {}        
+    if dataset=='train':
+        evecs = targets_train[i_source]['target_evecs']
+        evecs_trans = targets_train[i_source]['target_evecs_trans']
+        shot = targets_train[i_source]['target_shot']
+        evals = targets_train[i_source]['target_evals']
+        input_data['source_evecs'] = evecs
+        input_data['source_evecs_trans'] = evecs_trans
+        input_data['source_shot'] = shot
+        input_data['source_evals'] = evals
+        input_data.update(targets_train[i_target])
+       
+    elif dataset=='test':
+        evecs = targets_test[i_source]['target_evecs']
+        evecs_trans = targets_test[i_source]['target_evecs_trans']
+        shot = targets_test[i_source]['target_shot']
+        evals = targets_test[i_source]['target_evals']
+        input_data['source_evecs'] = evecs
+        input_data['source_evecs_trans'] = evecs_trans
+        input_data['source_shot'] = shot
+        input_data['source_evals'] = evals
+        input_data.update(targets_test[i_target])   
+        
+    return input_data
+
+def load_targets_to_ram():
+    global targets_train,targets_test
+    targets_train,targets_test = ({},{})    
+    targets_train = load_subs(train_subjects, main_dir, vert_dir,files_name)
+    targets_test= load_subs(test_subjects,main_dir_te, vert_dir_te,files_name_te)
+
+    
+def load_subs(subjects_list, dir_name,v_dir,f_name): 
+    targets = {}
+    
+    for i_target in subjects_list:             
+        target_file = dir_name +f_name +'%.4d.mat' % (i_target)
+        #print(target_file)
+        vert_file = v_dir +f_name +'%.4d.mat' % (i_target)   
+        input_data = sio.loadmat(target_file)        
+        evecs = input_data['target_evecs'][:, 0:FLAGS.num_evecs]
+        evecs_trans = input_data['target_evecs_trans'][0:FLAGS.num_evecs,:]
+        evals = input_data['target_evals'][0:FLAGS.num_evecs]        
+        input_data['target_evecs'] = evecs
+        input_data['target_evecs_trans'] = evecs_trans
+        input_data['target_evals'] = evals 
+        p_feat = sio.loadmat(vert_file)
+        input_data['target_shot'] =[]        
+        input_data['target_shot'] = p_feat['VERT']             
+        targets[i_target] = input_data        
+    return targets
+
+def get_bn_decay(batch):    
+    bn_momentum = tf.train.exponential_decay(
+                      BN_INIT_DECAY,
+                      batch*BATCH_SIZE,
+                      BN_DECAY_DECAY_STEP,
+                      BN_DECAY_DECAY_RATE,
+                      staircase=True)
+    bn_decay = tf.minimum(BN_DECAY_CLIP, 1 - bn_momentum)
+    return bn_decay 
+
+
+       
+def run_training():
+    
+    print('log_dir=%s' % FLAGS.log_dir_)
+    if not os.path.isdir(FLAGS.log_dir_):
+        os.makedirs(FLAGS.log_dir_)
+    if not os.path.isdir(FLAGS.matches_dir):
+        os.makedirs(FLAGS.matches_dir)
+    print('num_evecs=%d' % FLAGS.num_evecs)
+
+    print('building graph...')
+    
+    with tf.Graph().as_default():
+        
+        batch = tf.Variable(0)
+        bn_decay = get_bn_decay(batch)
+		
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+            #optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate)
+            #train_op = optimizer.minimize(net_loss,global_step=global_step, aggregation_method=2)
+        our_model = surfmnet(SURFMNetConfig)
+        optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(our_model.cost,global_step=global_step)		   
+        #saver = tf.train.Saver(tf.global_variables())   
+                
+        print('starting session...')
+        iteration = 0
+        init = tf.global_variables_initializer()
+
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+# Launch the graph
+        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+            sess.run(init)    
+        #with sv.managed_session(config=config) as sess:
+            print('loading data to ram...')
+            load_targets_to_ram()
+            
+            print('starting training loop...')
+            while iteration < FLAGS.max_train_iter:               
+                iteration += 1
+                start_time = time.time()
+                input_data = get_input_pair(FLAGS.batch_size, FLAGS.num_vertices, 'train')
+                           
+                _, cost_val = sess.run([optimizer,our_model.cost],feed_dict={our_model.pc_s: input_data['source_shot'],our_model.pc_t: input_data['target_shot'], \
+                                           our_model.source_evecs: input_data['source_evecs'], our_model.source_evecs_trans: input_data['source_evecs_trans'],
+                    our_model.source_evals: input_data['source_evals'], our_model.target_evecs: input_data['target_evecs'],
+                    our_model.target_evecs_trans: input_data['target_evecs_trans'], our_model.target_evals: input_data['target_evals'], our_model.phase:True})
+         
+                
+                duration = time.time() - start_time
+                print('train -: loss = %.2f (%.3f sec)'% ( cost_val, duration))
+                
+                if iteration%2000==0:                    
+                    for i_source in range(60,69):     
+                        for i_target in range(i_source+1,70):
+                            
+                            t = time.time()
+                            
+                            input_data = get_input_pair_test(i_target, i_source, 'test')
+                            source_evecs_ = input_data['source_evecs'][:, 0:FLAGS.num_evecs]
+                            target_evecs_ = input_data['target_evecs'][:, 0:FLAGS.num_evecs]
+        
+                            feed_dict = {
+                                phase: False,
+                                source_shot: [input_data['source_shot']],
+                                target_shot: [input_data['target_shot']],
+                                source_evecs: [input_data['source_evecs']],
+                                source_evecs_trans: [input_data['source_evecs_trans']],
+                                source_evals: [input_data['source_evals']],
+                                target_evecs: [input_data['target_evecs']],
+                                target_evecs_trans: [input_data['target_evecs_trans']],
+                                target_evals: [input_data['target_evals']]
+                                }
+                
+                            C_est_ = sess.run([C], feed_dict=feed_dict)                            
+                            Ct = np.squeeze(C_est_).T #Keep transposed
+                
+                            kdt = cKDTree(np.matmul(source_evecs_, Ct))
+                            
+                            dist, indices = kdt.query(target_evecs_, n_jobs=-1)
+                            indices = indices + 1
+                
+                            print("Computed correspondences for pair: %s, %s." % (i_source, i_target) +
+                                  " Took %f seconds" % (time.time() - t))                
+                            params_to_save = {}
+                            params_to_save['matches'] = indices
+                            #params_to_save['C'] = Ct.T
+                            # For Matlab where index start at 1  
+                            new_dir = FLAGS.matches_dir + '%.3d-' % iteration + '/'
+                            
+                            if not os.path.isdir(new_dir):
+                                print('matches_dir=%s' % new_dir)        
+                                os.makedirs(new_dir)
+                            
+                            sio.savemat(new_dir +FLAGS.files_name_te + '%.4d-' % i_source + FLAGS.files_name_te + '%.4d.mat' % i_target, params_to_save)
+                    
+#        
+            #saver.save(sess, FLAGS.log_dir_ + '/model.ckpt', global_step=step)
+            #writer.flush()
+            #sv.request_stop()
+            #sv.stop()
+
+
+def main(_):
+    import time
+    start_time = time.time()
+    run_training()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
